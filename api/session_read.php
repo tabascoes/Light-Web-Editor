1e67d710c8c567f6f463461b20f13d7b863fe341
tabascoes <87574268+tabascoes@users.noreply.github.com>
2021-09-19 23:22:44 +0900

    Update session_read.php

--- api/session_read.php
+++ api/session_read.php
@@ -1,8 +1,11 @@
 <?php
 session_start();
 $session = $_COOKIE['PHPSESSID']
+$randid = $_SESSION['randid'];
 $id = $_SESSION['id'];
 $key = $_SESSION['key'];
-
 $ip = $_SERVER['REMOTE_ADDR'];
+
+// Need to change iv
+openssl_decrypt($id, "AES-256-iv", $key, 0, "" );
 ?>
