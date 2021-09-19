d810b330e25a8abf214fd0ca3a2130bbe7572d57
tabascoes <87574268+tabascoes@users.noreply.github.com>
2021-09-19 23:08:13 +0900

    Update session_read.php

--- api/session_read.php
+++ api/session_read.php
@@ -1,12 +1,7 @@
 <?php
 $session = $_COOKIE['PHPSESSID']
-$id = $_COOKIE["id"];
-$key = $_COOKIE["key"];
+$id = $_SESSION['id'];
+$key = $_SESSION['key'];
 
 $ip = $_SERVER['REMOTE_ADDR'];
-
-	
-setcookie("id" , "" , time()-100 );
-setcookie("key" , "" , time()-100 );
-session_regenerate_id();
 ?>
