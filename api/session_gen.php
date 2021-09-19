e3c1835a9b3faf9401148d964eb434b76da4dd83
tabascoes <87574268+tabascoes@users.noreply.github.com>
2021-09-19 23:14:58 +0900

    Update session_gen.php

--- api/session_gen.php
+++ api/session_gen.php
@@ -7,7 +7,12 @@ unset($_SESSION['key']);
 $ip = $_SERVER['REMOTE_ADDR'];
 $ipint = var_dump(ip2long($ip));
 
-$id = "";
+function random($length = 8)
+{
+    return array_reduce(range(1, $length), function($p){ return $p.str_shuffle('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789')[0]; });
+}
+
+$id = random(100);
 $key = $ipint
 
 $_SESSION['id'] = $id;
