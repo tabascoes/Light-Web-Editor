6ce4b548ab2c4d0b789e5e68db95d110d6ff673e
tabascoes <87574268+tabascoes@users.noreply.github.com>
2021-09-19 23:20:56 +0900

    Update session_gen.php

--- api/session_gen.php
+++ api/session_gen.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 session_regenerate_id();
+unset($_SESSION['randid']);
 unset($_SESSION['id']);
 unset($_SESSION['key']);
 
@@ -12,9 +13,13 @@ function random($length = 8)
     return array_reduce(range(1, $length), function($p){ return $p.str_shuffle('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789')[0]; });
 }
 
-$id = random(100);
+$randid = random(100);
 $key = $ipint
+    
+// Need to set iv
+$id = openssl_encrypt( $randid, "AES-256-iv", $key, 0, "" );
 
+$_SESSION['randid'] = $randid;
 $_SESSION['id'] = $id;
 $_SESSION['key'] = $key;
 ?>
